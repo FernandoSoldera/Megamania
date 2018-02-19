6faaaf865e371ec61f957a1336988ad0b5c2c3f5
Fernando Soldera <fernandosoldera@hotmail.com>
2018-02-19 00:37:06 -0300

    First Commit

---
+++ Assets/Assets/Scripts/FireController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour {
+
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    
+	void Update () {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(new Vector3(Time.deltaTime * horizontalSpeed, Time.deltaTime * verticalSpeed, 0));
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(new Vector3(Time.deltaTime * -horizontalSpeed, Time.deltaTime * verticalSpeed, 0));
+        }
+        else
+        {
+            transform.Translate(new Vector3(0, Time.deltaTime * verticalSpeed, 0));
+        }
+    }
+}
---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public GameObject fire;
+    public float fireTime;
+    private float timeCanFire;
+
+    void Start()
+    {
+        timeCanFire = 0;
+    }
+
+    void Update() {
+        if (Input.GetButton("Fire1"))
+        {
+            if (Time.time > timeCanFire)
+            {
+                Shoot();
+                timeCanFire = Time.time + fireTime;
+            }
+        }
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            transform.Translate(new Vector3(Time.deltaTime * speed, 0, 0));
+        }
+        else if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            transform.Translate(new Vector3(Time.deltaTime * -speed, 0, 0));
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(fire, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), transform.rotation);
+    }
+}
